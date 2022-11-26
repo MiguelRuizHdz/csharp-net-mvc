717d1cde15fbac290b12a346ed85a51b578f2721
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-26 14:27:28 -0600

    Clase 1

---
+++ Practicas/Clase1/Test/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Bienvenido a ASP.NET MVC.";
+            return View();
+        }
+        
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Mensaje desde el controlador en el momento del partido Mexico vs Argentina";
+        //    return View();
+        //}
+
+        //public HttpStatusCodeResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return new HttpStatusCodeResult(404);
+        //}
+
+        //public ContentResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return Content("Miguel Ruiz");
+        //}
+
+        //public JsonResult About()
+        //{
+        //    var persona1 = new Persona() { Nombre="Miguel", Edad=10};
+        //    var persona2 = new Persona() { Nombre="Juan", Edad=20};
+        //    List<Persona> Lista = new List<Persona>();
+        //    Lista.Add(persona1);
+        //    Lista.Add(persona2);
+
+        //    return Json(Lista, JsonRequestBehavior.AllowGet);
+        //}
+
+        //public RedirectResult About()
+        //{
+        //    return Redirect("https://www.google.mx");
+        //}
+
+        //public RedirectToRouteResult About()
+        //{
+        //    return RedirectToAction("About", "Home");
+        //}
+
+        //public HttpStatusCodeResult About()
+        //{
+        //    return new HttpStatusCodeResult(301);
+        //}
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
